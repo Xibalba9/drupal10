fa698ed2355d9a170b36069dc6bf705d754f9ecf
Xibalba9 <amirreza.ahmad@gmail.com>
2023-04-10 14:26:30 -0500

    Added the Created Module html5_audio

---
+++ web/modules/html5_audio/src/Plugin/Field/FieldFormatter/Html5AudioFieldFormatter.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\html5_audio\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'HTML5 Audio' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "html5_audio_formatter",
+ *   label = @Translation("HTML5 Audio"),
+ *   field_types = {
+ *     "link"
+ *   }
+ * )
+ */
+class Html5AudioFieldFormatter extends FormatterBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings()
+  {
+    return [
+      'foo' => 'bar',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state)
+  {
+
+    $elements['foo'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Foo'),
+      '#default_value' => $this->getSetting('foo'),
+    ];
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode)
+  {
+    $element = [];
+
+    // Render all field values as part of a single <audio> tag.
+    $sources = [];
+    foreach ($items as $item) {
+      // Get the mime type.
+      $mimetype = \Drupal::service('file.mime_type.guesser')->guessMimeType($item->uri);
+      $sources[] = [
+        'src' => $item->uri,
+        'mimetype' => $mimetype,
+      ];
+    }
+
+    // Put everything in an array for theming.
+    $element[] = [
+      '#theme' => 'audio_tag',
+      '#sources' => $sources,
+    ];
+
+    dpm($element);
+
+    return $element;
+  }
+}
