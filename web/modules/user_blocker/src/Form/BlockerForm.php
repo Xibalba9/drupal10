b373f15ff6396f185f55526d1663ae0f5830a1f6
Xibalba9 <amirreza.ahmad@gmail.com>
2023-04-16 19:42:49 -0500

    Added auto compelete and menu link for User Blocker Module

--- web/modules/user_blocker/src/Form/BlockerForm.php
+++ web/modules/user_blocker/src/Form/BlockerForm.php
@@ -4,6 +4,7 @@ namespace Drupal\user_blocker\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\user\Entity\User;
 
 /**
  * Provides a User blocker form.
@@ -21,31 +22,45 @@ class BlockerForm extends FormBase
 
   /**
    * {@inheritdoc}
+   * 
+   * This is the autocomplete version.
    */
   public function buildForm(array $form, FormStateInterface $form_state)
   {
 
-    // $form['message'] = [
-    //   '#type' => 'textarea',
-    //   '#title' => $this->t('Message'),
-    //   '#required' => TRUE,
-    // ];
+      // $form['message'] = [
+      //   '#type' => 'textarea',
+      //   '#title' => $this->t('Message'),
+      //   '#required' => TRUE,
+      // ];
 
-    // $form['actions'] = [
-    //   '#type' => 'actions',
-    // ];
-    // $form['actions']['submit'] = [
-    //   '#type' => 'submit',
-    //   '#value' => $this->t('Send'),
+      // $form['actions'] = [
+      //   '#type' => 'actions',
+      // ];
+      // $form['actions']['submit'] = [
+      //   '#type' => 'submit',
+      //   '#value' => $this->t('Send'),
 
-      $form['username'] = [
-        '#type' => 'textfield',
-        '#required' => true,
+      // $form['username'] = [
+      //   '#type' => 'textfield',
+      //   '#required' => true,
+      //   '#title' => $this->t('Username'),
+      //   '#description' => $this->t('Enter the username of the user you want to block.'),
+      //   '#maxlength' => 64,
+      //   '#size' => 64,
+      //   '#weight' => '0',
+      // ];
+      // $form['submit'] = [
+      //   '#type' => 'submit',
+      //   '#value' => $this->t('Submit'),
+      // ];
+
+      $form['uid'] = [
         '#title' => $this->t('Username'),
+        '#type' => 'entity_autocomplete',
+        '#target_type' => 'user',
+        '#required' => true,
         '#description' => $this->t('Enter the username of the user you want to block.'),
-        '#maxlength' => 64,
-        '#size' => 64,
-        '#weight' => '0',
       ];
       $form['submit'] = [
         '#type' => 'submit',
@@ -57,6 +72,8 @@ class BlockerForm extends FormBase
 
   /**
    * {@inheritdoc}
+   * 
+   * This is the autocomplete version.
    */
   public function validateForm(array &$form, FormStateInterface $form_state)
   {
@@ -66,46 +83,65 @@ class BlockerForm extends FormBase
 
     parent::validateForm($form, $form_state);
 
-    $username = $form_state->getValue('username');
-    $user = user_load_by_name($username);
-    if (empty($user) || !$username) {
-      $form_state->setError(
-        $form['username'],
-        $this->t('User %username was not found.', ['%username' => $username])
-      );
-    }
-    elseif ($user->id() === "0") {
+    // $username = $form_state->getValue('username');
+    // $user = user_load_by_name($username);
+    // if (empty($user) || !$username) {
+    //   $form_state->setError(
+    //     $form['username'],
+    //     $this->t('User %username was not found.', ['%username' => $username])
+    //   );
+    // }
+    // elseif ($user->id() === "0") {
 
-      $form_state->setError(
-        $form['username'],
-        $this->t('You need to enter the value!')
-      );
+    //   $form_state->setError(
+    //     $form['username'],
+    //     $this->t('You need to enter the value!')
+    //   );
 
-    }
-    else {
-      $current_user = \Drupal::currentUser();
-      if ($user->id() == $current_user->id()) {
-        $form_state->setError(
-          $form['username'],
-          $this->t('You cannot block your own account.')
-        );
-      }
-    }
+    // }
+    // else {
+    //   $current_user = \Drupal::currentUser();
+    //   if ($user->id() == $current_user->id()) {
+    //     $form_state->setError(
+    //       $form['username'],
+    //       $this->t('You cannot block your own account.')
+    //     );
+    //   }
+    // }
+
+    $uid = $form_state->getValue('uid');
+    $current_user = \Drupal::currentUser();
+    if ($uid == $current_user->id()){
+      $form_state->setError(
+        $form['uid'],
+        $this->t('You cannot block your own account.')
+      );
+    }     
+
   }
 
   /**
    * {@inheritdoc}
+   * 
+   * This the autocomplete version.
    */
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
     // $this->messenger()->addStatus($this->t('The message has been sent.'));
     // $form_state->setRedirect('<front>');
 
-    $username = $form_state->getValue('username');
-    $user = user_load_by_name($username);
+    // $username = $form_state->getValue('username');
+    // $user = user_load_by_name($username);
+    // $user->block();
+    // $user->save();
+    // $this->messenger()->addMessage($this->t('User ↩ %username has been blocked.', ['%username' => ↩ $user->getAccountName()]));
+
+
+    $uid = $form_state->getValue('uid');
+    $user = User::load($uid);
     $user->block();
     $user->save();
-    $this->messenger()->addMessage($this->t('User %username has been blocked.', ['%username' => $user->getAccountName()]));
+    $this->messenger()->addMessage($this->t('User @username has been blocked.', ['@username' => $user->getAccountName()]));
 
   }
 }
